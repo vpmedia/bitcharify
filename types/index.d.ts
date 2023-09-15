582e34ff9650bf1c4ebe25759acd3542d1681384
Andras Csizmadia <andras@vpmedia.hu>
2023-09-15 15:01:06 +0200

    Updated type checking config

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,9 @@
-import { generateBitmapFont } from "./bitcharify/generateBitmapFont";
-import { generateBitmapFonts } from "./bitcharify/generateBitmapFonts";
-import { CHARS_ALPHA } from "./bitcharify/core/const";
-import { CHARS_ALPHANUMERIC } from "./bitcharify/core/const";
-import { CHARS_ASCII } from "./bitcharify/core/const";
-import { CHARS_NUMERIC } from "./bitcharify/core/const";
-import { loadImage } from "./bitcharify/core/loadImage";
+import { generateBitmapFont } from "./bitcharify/generateBitmapFont.js";
+import { generateBitmapFonts } from "./bitcharify/generateBitmapFonts.js";
+import { CHARS_ALPHA } from "./bitcharify/core/const.js";
+import { CHARS_ALPHANUMERIC } from "./bitcharify/core/const.js";
+import { CHARS_ASCII } from "./bitcharify/core/const.js";
+import { CHARS_NUMERIC } from "./bitcharify/core/const.js";
+import { loadImage } from "./bitcharify/core/loadImage.js";
 export { generateBitmapFont, generateBitmapFonts, CHARS_ALPHA, CHARS_ALPHANUMERIC, CHARS_ASCII, CHARS_NUMERIC, loadImage };
 //# sourceMappingURL=index.d.ts.map
