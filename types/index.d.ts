18fee04e111672a2f4b0c6f82497da652de8aed2
Andras Csizmadia <andras@vpmedia.hu>
2023-09-16 19:49:40 +0200

    Improve code quality (jsdoc import quotes)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,9 @@
-import { generateBitmapFont } from "./bitcharify/generateBitmapFont.js";
-import { generateBitmapFonts } from "./bitcharify/generateBitmapFonts.js";
-import { CHARS_ALPHA } from "./bitcharify/core/const.js";
-import { CHARS_ALPHANUMERIC } from "./bitcharify/core/const.js";
-import { CHARS_ASCII } from "./bitcharify/core/const.js";
-import { CHARS_NUMERIC } from "./bitcharify/core/const.js";
-import { loadImage } from "./bitcharify/core/loadImage.js";
+import { generateBitmapFont } from './bitcharify/generateBitmapFont.js';
+import { generateBitmapFonts } from './bitcharify/generateBitmapFonts.js';
+import { CHARS_ALPHA } from './bitcharify/core/const.js';
+import { CHARS_ALPHANUMERIC } from './bitcharify/core/const.js';
+import { CHARS_ASCII } from './bitcharify/core/const.js';
+import { CHARS_NUMERIC } from './bitcharify/core/const.js';
+import { loadImage } from './bitcharify/core/loadImage.js';
 export { generateBitmapFont, generateBitmapFonts, CHARS_ALPHA, CHARS_ALPHANUMERIC, CHARS_ASCII, CHARS_NUMERIC, loadImage };
 //# sourceMappingURL=index.d.ts.map
